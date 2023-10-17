d4702a4caf3115dc65e514d97f35d95f3a526151
Jin Geonwoo <jinkonu99@gmail.com>
2023-10-18 02:15:56 +0900

    스프링부트 기본 프로젝트 업로드

---
+++ movlog/src/main/java/nora/movlog/MovlogApplication.java
@@ -0,0 +1,13 @@
+package nora.movlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovlogApplication.class, args);
+    }
+
+}
