5674cd997505bf125020253040f09a403a1557c3
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-02 15:50:55 +0900

    feat: StringConstant 추가 후 API key, url 이동

---
+++ src/main/java/nora/movlog/domain/constant/StringConstant.java
@@ -0,0 +1,14 @@
+package nora.movlog.domain.constant;
+
+public class StringConstant {
+    // KOBIS API
+    static String kobisKey = "4c9099b4c7f44c7a34192082ead54dbe";
+    static String kobisUrl = "http://www.kobis.or.kr";
+    static String kobisSearchByIdUrlPath = "/kobisopenapi/webservice/rest/movie/searchMovieInfo.json";
+    static String kobisSearchByNameUrlPath = "/kobisopenapi/webservice/rest/movie/searchMovieList.json";
+
+    // TMDB API
+    static String tmdbKey = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMDk1YTE3ZWEzYmNmZDBmZGUyYzliYWQ5MzhiZjE4MiIsInN1YiI6IjY1NDMzZDllZWQyYWMyMDBhZWIwNGNlYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KPotpNx48WIbRzopB-5n2AUfekStI5LqGuI2mjV8pk4";
+    static String tmdbUrl = "https://api.themoviedb.org";
+    static String tmdbSearchByQueryUrlPath = "/3/search/movie";
+}
