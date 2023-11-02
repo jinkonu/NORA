3a01040d352ee766913d235c1a25b7acd80ab5a4
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-02 15:57:28 +0900

    refactor: StringConstant 내 필드 public으로 변경

--- src/main/java/nora/movlog/domain/constant/StringConstant.java
+++ src/main/java/nora/movlog/domain/constant/StringConstant.java
@@ -2,13 +2,13 @@ package nora.movlog.domain.constant;
 
 public class StringConstant {
     // KOBIS API
-    static String kobisKey = "4c9099b4c7f44c7a34192082ead54dbe";
-    static String kobisUrl = "http://www.kobis.or.kr";
-    static String kobisSearchByIdUrlPath = "/kobisopenapi/webservice/rest/movie/searchMovieInfo.json";
-    static String kobisSearchByNameUrlPath = "/kobisopenapi/webservice/rest/movie/searchMovieList.json";
+    public static String kobisKey = "4c9099b4c7f44c7a34192082ead54dbe";
+    public static String kobisUrl = "http://www.kobis.or.kr";
+    public static String kobisSearchByIdUrlPath = "/kobisopenapi/webservice/rest/movie/searchMovieInfo.json";
+    public static String kobisSearchByNameUrlPath = "/kobisopenapi/webservice/rest/movie/searchMovieList.json";
 
     // TMDB API
-    static String tmdbKey = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMDk1YTE3ZWEzYmNmZDBmZGUyYzliYWQ5MzhiZjE4MiIsInN1YiI6IjY1NDMzZDllZWQyYWMyMDBhZWIwNGNlYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KPotpNx48WIbRzopB-5n2AUfekStI5LqGuI2mjV8pk4";
-    static String tmdbUrl = "https://api.themoviedb.org";
-    static String tmdbSearchByQueryUrlPath = "/3/search/movie";
+    public static String tmdbKey = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMDk1YTE3ZWEzYmNmZDBmZGUyYzliYWQ5MzhiZjE4MiIsInN1YiI6IjY1NDMzZDllZWQyYWMyMDBhZWIwNGNlYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KPotpNx48WIbRzopB-5n2AUfekStI5LqGuI2mjV8pk4";
+    public static String tmdbUrl = "https://api.themoviedb.org";
+    public static String tmdbSearchByQueryUrlPath = "/3/search/movie";
 }
