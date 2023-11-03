9e3cc4d066d3a662e44117210e3eb9994fd79d9f
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-03 21:29:08 +0900

    refactor: MovieService에서 도메인 생성하도록 수정

--- src/main/java/nora/movlog/service/MovieService.java
+++ src/main/java/nora/movlog/service/MovieService.java
@@ -1,16 +1,24 @@
 package nora.movlog.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import nora.movlog.constant.NumberConstant;
-import nora.movlog.domain.Movie;
-import nora.movlog.repository.MovieKobisApiRepository;
+import nora.movlog.domain.*;
+import nora.movlog.dto.MovieTmdbDto;
 import nora.movlog.repository.MovieJpaRepository;
+import nora.movlog.repository.MovieTmdbApiRepository;
+import nora.movlog.repository.interfaces.ActorRepository;
+import nora.movlog.repository.interfaces.DirectorRepository;
+import nora.movlog.repository.interfaces.GenreRepository;
+import nora.movlog.repository.interfaces.NationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.awt.image.DirectColorModel;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
+import static nora.movlog.constant.NumberConstant.*;
 
 /*
 영화 서비스
@@ -21,38 +29,33 @@ import java.util.List;
 @RequiredArgsConstructor
 @Service
 public class MovieService {
-    private final MovieKobisApiRepository movieKobisApiRepository;
     private final MovieJpaRepository movieJpaRepository;
+    private final MovieTmdbApiRepository movieTmdbApiRepository;
 
-    // KOBIS "movieCd"를 기반으로 KOBIS API에 질의해서 검색
-    public Movie findByKobisId(String kobisId) {
-        Movie movie = null;
-
-        try {
-            movie = movieKobisApiRepository.findByKobisId(kobisId);
-        } catch (IOException e) {
-            // 키 값 기반 검색이기 때문에 예외가 발생할 경우가 거의 없을듯
-        }
-
-        return movie;
-    }
+    private final NationRepository nationRepository;
+    private final GenreRepository genreRepository;
+    private final DirectorRepository directorRepository;
+    private final ActorRepository actorRepository;
 
     // 현재까지 영화 제목 기반으로만 검색 -> 1차적으로 DB 검색 -> 없거나 부족하면 KOBIS API에 질의
     @Transactional( readOnly = true )
     public List<Movie> search(String searchDt) {
         List<Movie> dbMovieList = movieJpaRepository.findByName(searchDt);
-        List<Movie> apiMovieList = new ArrayList<>();
+        List<MovieTmdbDto> tmdbDtos = new ArrayList<>();
 
-        if (dbMovieList.size() < NumberConstant.MIN_SEARCH_LIST_SIZE) {
+        if (dbMovieList.size() < MIN_SEARCH_LIST_SIZE) {
             try {
-                apiMovieList = movieKobisApiRepository.findByKobisString(searchDt);
-                join(apiMovieList);
-            } catch (IOException e) {
-                // 잘못된 이름을 검색할 경우 예외 발생 대신 검색결과가 없으므로 발생할 경우가 거의 없을듯
-            }
+                tmdbDtos = movieTmdbApiRepository.findByQuery(searchDt);
+            } catch (IOException e) { }
         }
 
-        dbMovieList.addAll(apiMovieList);
+        for (MovieTmdbDto dto : tmdbDtos)
+            if (findOne(dto.getId()) == null) {
+                Movie movie = createFromTmdbDto(dto);
+                dbMovieList.add(movie);
+                join(movie);
+            }
+
         return dbMovieList;
     }
 
@@ -64,10 +67,78 @@ public class MovieService {
 
     // DB에 신규 영화 저장
     @Transactional( readOnly = false )
-    public void join(List<Movie> movieList) {
-        for (Movie movie : movieList) {
-            if (movieJpaRepository.findById(movie.getId()) == null)
-                movieJpaRepository.save(movie);
+    public void join(Movie movie) {
+        movieJpaRepository.save(movie);
+    }
+    
+    // MovieTmdbDto -> Movie
+    @Transactional( readOnly = false )
+    public Movie createFromTmdbDto(MovieTmdbDto dto) {
+        // 연관관계 없는 필드만 채움
+        Movie movie = Movie.createFromTmdbDto(dto);
+        
+        // 연관관계 있는 필드 채워야 함
+        movie.setNations(fillNations(dto.getNation()));
+        movie.setGenre(fillGenres(dto.getGenres()));
+        movie.setDirectors(fillDirectors(dto.getDirectors()));
+        movie.setActors(fillActors(dto.getActors()));
+
+        return movie;
+    }
+
+    // Nation 조회해서 채워줌
+    @Transactional( readOnly = true )
+    public Set<Nation> fillNations(Set<String> ids) {
+        Set<Nation> nations = new HashSet<>();
+
+        for (String id : ids) {
+            Optional<Nation> nation = nationRepository.findById(id);
+            if (nation.isPresent()) nations.add(nation.get());
+            else nationRepository.save(new Nation(id));
         }
+
+        return nations;
+    }
+
+    // Genre 조회해서 채워줌
+    @Transactional( readOnly = true )
+    public Set<Genre> fillGenres(Set<String> ids) {
+        Set<Genre> genres = new HashSet<>();
+
+        for (String id : ids) {
+            Optional<Genre> genre = genreRepository.findById(id);
+            if (genre.isPresent()) genres.add(genre.get());
+            else genreRepository.save(new Genre(id));
+        }
+
+        return genres;
+    }
+
+    // Director 조회해서 채워줌
+    @Transactional( readOnly = true )
+    public Set<Director> fillDirectors(Set<String> ids) {
+        Set<Director> directors = new HashSet<>();
+
+        for (String id : ids) {
+            Optional<Director> director = directorRepository.findById(id);
+            if (director.isPresent()) directors.add(director.get());
+            else directorRepository.save(new Director(id));
+        }
+
+        return directors;
+    }
+
+    // Actor 조회해서 채워줌
+    @Transactional( readOnly = true )
+    public Set<Actor> fillActors(Set<String> ids) {
+        Set<Actor> actors = new HashSet<>();
+
+        for (String id : ids) {
+            Optional<Actor> actor = actorRepository.findById(id);
+            if (actor.isPresent()) actors.add(actor.get());
+            else actorRepository.save(new Actor(id));
+        }
+
+        return actors;
     }
 }
