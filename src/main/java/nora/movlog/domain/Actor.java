6b1392a625bc730f994745a3d946fd60b1f1d460
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-03 21:26:41 +0900

    feat: ActorRepository 추가 및 식별자 String으로 변경

--- src/main/java/nora/movlog/domain/Actor.java
+++ src/main/java/nora/movlog/domain/Actor.java
@@ -1,7 +1,6 @@
 package nora.movlog.domain;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,14 +11,12 @@ import lombok.NoArgsConstructor;
 public class Actor {
     /* 데이터 관리 및 조회용 */
     @Id
-    @GeneratedValue
-    private Long id;        // DB ID
-
+    private String id;      // DB ID
 
     /* 배우 데이터 */
     private String name;    // 한국 이름
 
-    public Actor(String name) {
-        this.name = name;
+    public Actor(String id) {
+        this.id = id;
     }
 }
