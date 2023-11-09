f166d5d422e2132a133074f7f5a11b27ff0f4783
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-09 16:44:47 +0900

    refactor: Genre 도메인 식별자를 int 형으로 변경

--- src/main/java/nora/movlog/domain/movie/Genre.java
+++ src/main/java/nora/movlog/domain/movie/Genre.java
@@ -13,9 +13,12 @@ import lombok.NoArgsConstructor;
 public class Genre {
     /* 데이터 관리 및 조회용 */
     @Id
-    private String id;        // DB ID
+    private Integer id;        // DB ID
 
-    public static Genre create(String id) {
-        return new Genre(id);
+    /* 장르 데이터 */
+    private String name;       // 한국 이름
+
+    public static Genre create(Integer id, String name) {
+        return new Genre(id, name);
     }
 }
