83bcda9d7eba84cbf33a396b366b478d2421d232
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-20 21:06:09 +0900

    fix: login 페이지 자동 리다이렉트 버그 해결

--- src/main/java/nora/movlog/config/SecurityConfig.java
+++ src/main/java/nora/movlog/config/SecurityConfig.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -20,5 +22,16 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest()
+                .permitAll();
+
+        return http.build();
     }
 }
