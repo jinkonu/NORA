9e5ad42a92eeb9a6f89d6cdf53a19596341db9d8
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-20 17:40:29 +0900

    refactor: 영화 검색 페이지 기반으로 변경

--- src/main/java/nora/movlog/repository/user/PostRepository.java
+++ src/main/java/nora/movlog/repository/user/PostRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    // FOR USER
+    // FOR MEMBER
     List<Post> findAllByMemberLoginId(String loginId);
 
     // FOR MOVIE
--- src/test/java/nora/movlog/service/MovieServiceTest.java
+++ src/test/java/nora/movlog/service/MovieServiceTest.java
@@ -11,17 +11,20 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import static nora.movlog.domain.constant.NumberConstant.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SuppressWarnings("ALL")
 @Transactional
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class MovieServiceTest {
+
+    private static final int DEFAULT_SEARCH_PAGE = 0;
+    private static final int DEFAULT_SEARCH_SIZE = 5;
 
     @Autowired
     MovieService movieService;
@@ -29,10 +32,10 @@ class MovieServiceTest {
     @Rollback( value = false )
     @Test
     @DisplayName("문자열 기반 검색")
-    void search_문자열_기반_검색() throws IOException {
+    void search_문자열_기반_검색() {
         String searchParam = "보이";
 
-        List<Movie> searchResult = movieService.search(searchParam);
+        List<Movie> searchResult = movieService.search(searchParam, DEFAULT_SEARCH_PAGE, DEFAULT_SEARCH_SIZE);
 
         for (Movie movie : searchResult) {
             System.out.println("movie.getTitleKo() = " + movie.getTitleKo());
@@ -44,10 +47,10 @@ class MovieServiceTest {
     @Rollback( value = false )
     @Test
     @DisplayName("API 검색 결과 영화들을 DB에 저장")
-    void search_API_검색_결과_영화들을_DB에_저장() throws IOException {
+    void search_API_검색_결과_영화들을_DB에_저장() {
         String searchParam = "돼지";
 
-        List<Movie> searchResult = movieService.search(searchParam);
+        List<Movie> searchResult = movieService.search(searchParam, DEFAULT_SEARCH_PAGE, DEFAULT_SEARCH_SIZE);
         List<Movie> dbMovieList = new ArrayList<>();
 
         for (Movie movie : searchResult)
