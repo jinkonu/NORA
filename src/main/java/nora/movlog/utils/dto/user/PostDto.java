dfaefef44f19d4960a55eec59419875ec93d6a8c
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-25 20:17:59 +0900

    refactor: PostDto에서 날짜 문자열로 담도록 수정

--- src/main/java/nora/movlog/utils/dto/user/PostDto.java
+++ src/main/java/nora/movlog/utils/dto/user/PostDto.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import nora.movlog.domain.user.Post;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static nora.movlog.domain.constant.NumberConstant.*;
 
@@ -14,11 +15,12 @@ public class PostDto {
     /* Post */
     private Long id;
     private String body;
-    private LocalDateTime createdAt;
-    private LocalDateTime lastModifiedAt;
+    private String createdAt;
+    private String lastModifiedAt;
 
     /* Movie */
     private String movieId;
+    private String movieTitle;
 
     /* Member */
     private long memberId;
@@ -27,16 +29,20 @@ public class PostDto {
     /* Like */
     private Integer likeCnt;
 
+    /* Date */
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
     public static PostDto of(Post post) {
         return PostDto.builder()
                 .id(post.getId())
                 .body(post.getBody())
                 .movieId(post.getMovie().getId())
+                .movieTitle(post.getMovie().getTitleKo())
                 .memberId(post.getMember().getId())
                 .memberNickname(post.getMember().getNickname())
                 .likeCnt(DEFAULT_LIKE_CNT)
-                .createdAt(post.getCreatedAt())
-                .lastModifiedAt(post.getLastModifiedAt())
+                .createdAt(formatter.format(post.getCreatedAt()))
+                .lastModifiedAt(formatter.format(post.getLastModifiedAt()))
                 .build();
     }
 }
