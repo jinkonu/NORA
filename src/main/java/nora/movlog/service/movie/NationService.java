75c848b989977bf69970fc0c687069c11b502d1d
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-25 21:04:42 +0900

    refactor: Movie 관련 하위 엔티티들 DB 거치지 않는 형식으로 수정

--- src/main/java/nora/movlog/service/movie/NationService.java
+++ src/main/java/nora/movlog/service/movie/NationService.java
@@ -1,36 +1,18 @@
 package nora.movlog.service.movie;
 
-import lombok.RequiredArgsConstructor;
 import nora.movlog.domain.movie.Nation;
-import nora.movlog.repository.movie.interfaces.NationRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-@RequiredArgsConstructor
 @Service
 public class NationService {
-    private final NationRepository nationRepository;
-
-    @Transactional( readOnly = true )
     public Set<Nation> fillNations(Set<String> ids) {
-        Set<Nation> nations = new HashSet<>();
-
-        for (String id : ids) {
-            Optional<Nation> nation = nationRepository.findById(id);
-
-            if (nation.isPresent()) nations.add(nation.get());
-            else {
-                Nation savedNation = nationRepository.save(Nation.builder()
+        return ids.stream()
+                .map(id -> Nation.builder()
                         .id(id)
-                        .build());
-                nations.add(savedNation);
-            }
-        }
-
-        return nations;
+                        .build())
+                .collect(Collectors.toSet());
     }
 }
