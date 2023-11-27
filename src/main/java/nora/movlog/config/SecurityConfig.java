8d96075c9a9873c03e2c18cee097c8763c1f1c05
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-27 13:15:23 +0900

    refactor: SecurityConfig 내 URI를 상수로 이전

--- src/main/java/nora/movlog/config/SecurityConfig.java
+++ src/main/java/nora/movlog/config/SecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import static nora.movlog.utils.constant.StringConstant.*;
+
 /*
 * 사용자 로그인, 회원가입 등 보안 관련 config
 */
@@ -40,20 +42,20 @@ public class SecurityConfig {
                         .requestMatchers(new AntPathRequestMatcher("/icons/**"),
                                          new AntPathRequestMatcher("/img/**"),
                                          new AntPathRequestMatcher("/css/**")).permitAll() // 웹페이지 표시를 위한 리소스는 전체 공개
-                        .requestMatchers(new AntPathRequestMatcher("/login"),
-                                         new AntPathRequestMatcher("/join")).anonymous() // 로그인, 회원가입 페이지는 로그인하지 않은 회원에게만 보이게
+                        .requestMatchers(new AntPathRequestMatcher(LOGIN_URI),
+                                         new AntPathRequestMatcher(JOIN_URI)).anonymous() // 로그인, 회원가입 페이지는 로그인하지 않은 회원에게만 보이게
                         .anyRequest().authenticated() // 그 외 페이지는 로그인한 회원에게만 보이게
                 )
                 .formLogin(login -> login
-                        .loginPage("/login")
+                        .loginPage(LOGIN_URI)
                         .usernameParameter("loginId")
                         .passwordParameter("password")
-                        .defaultSuccessUrl("/search") // 이후 수정
-                        .failureUrl("/login")
+                        .defaultSuccessUrl(HOME_URI) // 이후 수정
+                        .failureUrl(LOGIN_URI)
                         .permitAll()
                 )
                 .logout(logout -> logout
-                        .logoutUrl("/")
+                        .logoutUrl(HOME_URI)
                         .invalidateHttpSession(true)
                         .deleteCookies("JSESSIONID")
                 )
