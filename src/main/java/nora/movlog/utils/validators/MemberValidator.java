065634aa2a25ea34ac80fb8be004380901f5b8cd
pww1101 <pww1101@gmail.com>
2023-11-27 15:00:39 +0900

    fix: 일부 constant 관련 수정

--- src/main/java/nora/movlog/utils/validators/MemberValidator.java
+++ src/main/java/nora/movlog/utils/validators/MemberValidator.java
@@ -26,7 +26,9 @@ public class MemberValidator {
         // loginId
         if (dto.getLoginId().isEmpty())
             bindingResult.addError(new FieldError("requestDto", "loginId", NO_LOGIN_ID_ERROR));
-        else if (dto.getLoginId().length() < MAX_LOGIN_LENGTH)
+        else if (dto.getLoginId().length() < MIN_LOGIN_LENGTH)
+            bindingResult.addError(new FieldError("requestDto", "loginId", TOO_SHORT_LOGIN_ID_ERROR));
+        else if (dto.getLoginId().length() > MAX_LOGIN_LENGTH)
             bindingResult.addError(new FieldError("requestDto", "loginId", TOO_LONG_LOGIN_ID_ERROR));
         else if (memberRepository.existsByLoginId(dto.getLoginId()))
             bindingResult.addError(new FieldError("requestDto", "loginId", DUPLICATE_LOGIN_ID_ERROR));
@@ -42,7 +44,7 @@ public class MemberValidator {
         // nickname
         if (dto.getNickname().isEmpty())
             bindingResult.addError(new FieldError("requestDto", "nickname", NO_NICKNAME_ERROR));
-        else if (dto.getNickname().length() < MAX_NICKNAME_LENGTH)
+        else if (dto.getNickname().length() > MAX_NICKNAME_LENGTH)
             bindingResult.addError(new FieldError("requestDto", "nickname", TOO_LONG_NICKNAME_ERROR));
 
         return bindingResult;
