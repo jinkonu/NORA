570fdaec9930cc8059fc6bb5dfa02d6026cf4a13
Jin Geonwoo <jinkonu99@gmail.com>
2023-12-01 17:46:13 +0900

    remove: 불필요한 리포지토리 삭제

--- src/main/java/nora/movlog/repository/movie/interfaces/DirectorRepository.java
+++
@@ -1,9 +0,0 @@
-package nora.movlog.repository.movie.interfaces;
-
-import nora.movlog.domain.movie.Director;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DirectorRepository extends JpaRepository<Director, String> {
-}
