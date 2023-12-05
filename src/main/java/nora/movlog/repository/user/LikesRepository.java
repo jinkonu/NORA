ff138beb57ceaf3a00272cfab9c539c62916b45b
Jin Geonwoo <jinkonu99@gmail.com>
2023-12-05 19:44:46 +0900

    refactor: 리포지토리 클래스에 어노테이션 추가

--- src/main/java/nora/movlog/repository/user/LikesRepository.java
+++ src/main/java/nora/movlog/repository/user/LikesRepository.java
@@ -3,9 +3,11 @@ package nora.movlog.repository.user;
 import nora.movlog.domain.user.Likes;
 import nora.movlog.domain.user.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface LikesRepository extends JpaRepository<Likes, Long> {
     void deleteByMemberLoginIdAndPostId(String memberLoginId, long postId);
     boolean existsByMemberLoginIdAndPostId(String memberId, long postId);
