c026f0db4520c11bfcc7b0c2479d142b4ac79c54
Jin Geonwoo <jinkonu99@gmail.com>
2023-12-05 17:54:46 +0900

    feat: 이미지 도메인 추가

---
+++ src/main/java/nora/movlog/domain/user/Image.java
@@ -0,0 +1,27 @@
+package nora.movlog.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Entity
+public class Image {
+
+    /* DB id */
+    @Id @GeneratedValue
+    private long id;
+
+    /* 이미지 데이터 */
+    private String originalFileName;
+    private String savedFileName;
+
+    /* 연관관계 */
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+}
