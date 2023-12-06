f07eff760cdcf8cb1614ab2bf9e37db0d89ef687
Jin Geonwoo <jinkonu99@gmail.com>
2023-12-06 13:20:51 +0900

    feat: 자기 프로필인지 확인하는 GET 매핑 메서드 추가

--- src/main/java/nora/movlog/controller/user/MemberController.java
+++ src/main/java/nora/movlog/controller/user/MemberController.java
@@ -40,9 +40,9 @@ public class MemberController {
 
     // 세션 기반 프로필
     @GetMapping
-    public String profilePageFrom(Authentication auth,
-                                  @RequestParam(defaultValue = DEFAULT_SEARCH_PAGE) int page,
+    public String profilePageFrom(@RequestParam(defaultValue = DEFAULT_SEARCH_PAGE) int page,
                                   @RequestParam(defaultValue = DEFAULT_SEARCH_SIZE) int size,
+                                  Authentication auth,
                                   Model model) {
         Member loginMember = memberService.findByLoginId(MemberFinder.getLoginId(auth));
 
@@ -51,5 +51,14 @@ public class MemberController {
         model.addAttribute("posts", postService.findAllFromMember(loginMember.getId(), page, size));
 
         return "userPage";
+    }
+
+
+    // 자기 프로필 화면인지 확인
+    @ResponseBody
+    @GetMapping(ID_URI + "/isSelf")
+    public boolean isSelf(@PathVariable long id,
+                          Authentication auth) {
+        return memberService.profile(id).equals(memberService.findByLoginId(MemberFinder.getLoginId(auth)));
     }
 }
